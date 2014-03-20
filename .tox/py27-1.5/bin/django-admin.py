a8bd7b8a752800dddea95eb34af63a3f198384b9
Numan S <numan856@gmail.com>
2014-03-20 14:32:51 -0700

    initial commit

---
+++ .tox/py27-1.5/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/numan/.virtualenvs/tastypie/sg-django-tastypie/.tox/py27-1.5/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
